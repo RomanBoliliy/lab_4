01c0fa2fd923e1fad2cb90230887129fa73a349c
RomanBoliliy <21fi.r.bolilyi@std.npu.edu.ua>
2023-12-09 21:03:18 +0200

    Added lab 4

---
+++ Data.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp1
+{
+    static internal class Data
+    {
+        private static SortedList<string, string> users = new SortedList<string, string>();
+
+        public static void add(string log, string pas) { 
+        users.Add(log, pas);
+            MessageBox.Show("Sign up correct");
+        }
+
+        public static void check(string log, string pas) {
+            int ID = -1;
+            for (int i = 0; i < users.Count; i++)
+            {
+                if (users.ContainsKey(log)){ ID = i; MessageBox.Show("Login correct!"); }
+            }
+            if (ID == -1) { MessageBox.Show("Login not correct!"); return; }
+            for (int i = 0;i < users.Count; i++)
+            {
+                
+                if (users.GetValueOrDefault(log) == pas) { MessageBox.Show("Password correct!"); return; }
+            }
+            MessageBox.Show("Can`t sign in");
+        }
+
+    }
+}
---
+++ SignUp.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для SignUp.xaml
+    /// </summary>
+    public partial class SignUp : Window
+    {
+
+       
+
+        public SignUp()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Button_SignUp_Click(object sender, RoutedEventArgs e)
+        {
+            string log = TextBoxLogin.Text.Trim();
+            string pas = PasswordBox.Password.Trim();
+            string pas2 = PasswordBox2.Password.Trim();
+            if (log.Length < 3) { MessageBox.Show("Занадто короткий логін"); PasswordBox2.Clear(); TextBoxLogin.Clear(); }
+            else if (log.Length > 10) { MessageBox.Show("Занадто довгий логін"); TextBoxLogin.Clear(); PasswordBox2.Clear(); }
+            else if(pas.Length < 3) { MessageBox.Show("Занадто короткий пароль"); PasswordBox.Clear(); PasswordBox2.Clear(); }
+            else if(pas.Length > 10) { MessageBox.Show("Занадто довгий пароль"); PasswordBox.Clear(); PasswordBox2.Clear(); }
+            else if(pas == pas2)
+            {
+
+                Data.add(log, pas);
+
+            }
+            else {
+                PasswordBox2.Clear();
+                MessageBox.Show("Паролі не співпадають"); 
+            }
+
+        }
+
+
+        private void Button_Back_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            Hide();
+        }
+
+    }
+}
